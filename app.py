0d22627cbd47e431464b58ab1c86af5c9ed65187
Pun Suwannasang <punpunkapaipai@gmail.com>
2026-03-01 18:46:26 +0700

    update app

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+
+# 1. ตั้งค่าหน้าเพจ
+st.set_page_config(page_title="My Simple Dashboard", layout="wide")
+st.title("📊 Sales Dashboard อย่างง่าย")
